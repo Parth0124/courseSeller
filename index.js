443e3203a838e302b0433a83c39054f6b03f4f74
Abhiraj412 <abhangparth@gmail.com>
2024-06-11 11:24:41 +0530

    middleware for user and admin authentication added

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ let ADMINS = [];  //array of admins
 let USERS = [];    //array of users
 let COURSES = [];   //array of courses
 
+const adminAuthentication = (req,res,next) => {
+    const { username, password } = req.headers;
+    const admin = ADMINS.find(a => a.username === username && a.password === password)
+    if(admin)
+        {
+            next();
+        }
+        else
+        {
+            res.status(403).json({message: "Admin authentication failed."})
+        }
+}
+
+const userAuthentication = (req,res,next) => {
+    const { username,password } = req.headers;
+    const user = USERS.find(u => u.username === username && u.password === password)
+    if(user)
+        {
+            req.user= user; //add user object to the request
+            next();
+        }
+        else
+        {
+            res.status(403).json({message: "User authentication failed."})
+        }
+}
+
 app.post('/admin/signup', (req, res) => {
     const admin = req.body;
     const existingAdmin = ADMINS.find(a => a.username === admin.username);
@@ -20,7 +47,7 @@ app.post('/admin/signup', (req, res) => {
         }
 });
 
-app.post('/admin/login',  adminAuthentication, (req,res) => {
+app.post('/admin/login',  adminAuthentication, (req,res) => {  //adminAuthentication is a midleware specific and exclusive only for login route. it is a route specific middleware which are sent right before the arrow fucntions or callback functions.
     res.json({message: "Logged in successfully"});
 });
 
