0d8c4a537cd7246bf4d6177e7d9dc4887e1c935c
Abhiraj412 <abhangparth@gmail.com>
2024-06-11 11:02:54 +0530

    array of users, admins and courses created

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let ADMINS = [];  //array of admins
+let USERS = [];    //array of users
+let COURSES = [];   //array of courses
+
